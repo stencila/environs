08b2fac3a7c15491371ade26a6c499bf1477cd77
Nokome Bentley <me@noko.me>
2017-07-17 16:04:11 +1200

    Move build stuff to .build

---
+++ .build/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Download the repo to get lastest versions of Dockerfiles
+rm -rf stencila-images*
+curl --location "https://github.com/stencila/images/tarball/master" > stencila-images.tar.gz
+tar -xzf stencila-images.tar.gz
+cd stencila-images-*
+
+# Build images
+. build.sh
+
+# Push images
+. push.sh
