954efddde8f4df9435584475e6893493975054e1
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-07-25 15:03:20 +1200

    DOCKER_FLAGS and automatic install of strace

--- .shrink/shrink-docker.sh
+++ .shrink/shrink-docker.sh
@@ -5,6 +5,9 @@ if [ $# -lt 3 ]; then
   echo "Strips IN_IMAGE down to just the files needed to run COMMAND."
   echo "Saves result to OUT_REPOISTORY and runs the command again to check."
   echo 
+  echo "If you need to specify other command line options for `docker run`"
+  echo "set the DOCKER_FLAGS environment variable."
+  echo 
   echo "WARNING: If different commands or input may not work on the new image."
   echo  	
   echo "  Usage: $0 IN_IMAGE OUT_REPOSITORY[:TAG] COMMAND [ARG...]"
@@ -13,25 +16,48 @@ fi
 
 function cleanup {
   if [ -n "$TEMPDIR" -a -d "$TEMPDIR" ]; then
-    chmod +w -R "$TEMPDIR"
-#    rm -fR "$TEMPDIR"
+    chmod -R +w "$TEMPDIR"
+    rm -fR "$TEMPDIR"
   fi
 }
 
 trap cleanup EXIT
 
-TEMPDIR=$(mktemp -d)
+
+# Create temporary directory
+if [ "$SHRINK_TEMP" == "" ]; then
+  TEMPDIR=$(mktemp -d)
+else
+  TEMPDIR=$(mktemp -d "$SHRINK_TEMP/shrink-docker.XXXXXX")
+fi
 
 IN_IMAGE=$1
 shift
 OUT_REPOSITORY=$1
 shift
 
+
+# Build docker image with strace
+if [[ "$IN_IMAGE" == *:* ]]; then
+  STRACE_TAG="$IN_IMAGE-strace"
+else
+  STRACE_TAG="$IN_IMAGE:strace"
+fi
+
+cd "$TEMPDIR"
+cat >Dockerfile <<HERE	
+FROM $IN_IMAGE
+
+RUN apt-get update && \\
+    apt-get -y --no-install-recommends install strace && \\
+    rm -rf /var/lib/apt/lists/*
+HERE
+
+docker build . -t $STRACE_TAG
+
 # Make sure stripdoc/strace.out is not created by docker (since then we may not have permission to delete it)
 touch "$TEMPDIR"/strace.out
-# docker run --rm -v "$TEMPDIR":/mnt/strace -v `pwd`:/code -v `pwd`/input:/input -v `pwd`/output:/output -v `pwd`/publish:/publish -w /code/talk-mpi-seven-by-seven-2017 --memory 1000000000b --memory-swap 1000000000b --cpu-period 25000 --cpu-quota 25000 -e PARAMETERS='{"env":null,"refresh":null,"memory":null,"docker":null,"cpus":null,"databases":null,"publish":null}' -e OUTPUTDIR=/output/ -e INPUTDIR=/input/ $IN_IMAGE strace -f -o /mnt/strace/strace.out "$@"
-docker run --rm -v "$TEMPDIR":/mnt/strace -v `pwd`:/code -v `pwd`/input:/input -v `pwd`/output:/output -w /code/ $IN_IMAGE strace -f -o /mnt/strace/strace.out "$@"
-
+docker run --rm -v "$TEMPDIR":/mnt/strace $DOCKER_FLAGS $STRACE_TAG strace -f -o /mnt/strace/strace.out "$@"
 
 # Make a sorted list of the files we need to keep based on the strace output
 read -r -d '' READLINKS <<-'HERE' || true
@@ -66,11 +92,7 @@ cat "$TEMPDIR"/exclude.txt > "$TEMPDIR"/exclude2.txt
 # Import the files back into the new repository (excluding those we want to leave out)
 (cd "$TEMPDIR"/files && (tar -c --exclude-from="$TEMPDIR"/exclude2.txt . | docker import - $OUT_REPOSITORY))
 
-echo Running
-# Check the command still runs
-#docker run --rm -v "$TEMPDIR":/mnt/strace $IN_IMAGE -v `pwd`:/code -v `pwd`/input:/input -v `pwd`/output:/output -v `pwd`/publish:/publish -w /code/talk-mpi-seven-by-seven-2017 --memory 1000000000b --memory-swap 1000000000b --cpu-period 25000 --cpu-quota 25000 -e PARAMETERS='{"env":null,"refresh":null,"memory":null,"docker":null,"cpus":null,"databases":null,"publish":null}' -e OUTPUTDIR=/output/ -e INPUTDIR=/input/ strace -f -o /mnt/strace/strace.out "$@"
-#docker run --rm $OUT_REPOSITORY "$@"
-
 echo
 echo "New image imported as '$OUT_REPOSITORY'."
+echo "To run it use: docker run --rm $DOCKER_FLAGS $OUT_REPOSITORY $@"
 
