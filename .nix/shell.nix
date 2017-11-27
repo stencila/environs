2835dc210daa69eefd08ab20c1f724c9e54537ba
Nokome Bentley <me@noko.me>
2017-11-27 16:39:44 +1300

    Add shellHook to define R_LIBS_USER

--- .nix/shell.nix
+++ .nix/shell.nix
@@ -16,6 +16,11 @@ let
     version = "1";
     buildCommand = "mkdir -p $out/stencila";
     buildInputs = inputs;
+    # R looks for packages in R_LIBS_USER which defaults to a location in the users HOME.
+    # This causes issues when using `nix-shell` if the user has R installed. 
+    # Here we define R_LIBS_USER so there is no clash. `/tmp` is used so that if the 
+    # user installs any new R packages while in the Nix shell, they are not persisted. 
+    shellHook = "export R_LIBS_USER=/tmp";
   };
 
 in shell
